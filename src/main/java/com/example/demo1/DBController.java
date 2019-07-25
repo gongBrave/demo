68c50464ff879bab7733685b5af54461dbd36236
admin <gong.yuming@163.com>
2019-07-25 11:35:24 +0800

    First commit all.

--- src/main/java/com/example/demo1/DBController.java
+++ src/main/java/com/example/demo1/DBController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//2.0你好吗
+
 @RestController
 @RequestMapping("/mydb")
 public class DBController {
